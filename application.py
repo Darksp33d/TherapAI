ba0c4a73d961a69e19ef43e28f6f8208b5ac5827
Darksp33d <durrani.saeed1@gmail.com>
2023-10-07 02:20:19 -0400

    Updated port

--- application.py
+++ application.py
@@ -43,4 +43,5 @@ def process_audio(file_path):
             return "API unavailable.", None
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
