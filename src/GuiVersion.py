7fabd4414e0e53379b61b0b38f496e89718e6e14
Darksp33d <durrani.saeed1@gmail.com>
2023-10-07 02:14:04 -0400

    Changed file structure

--- src/GuiVersion.py
+++ src/GuiVersion.py
@@ -1,23 +1,33 @@
 import requests
+import json
+import ssl
+from requests.packages.urllib3.util.ssl_ import create_urllib3_context
 
-# Replace this URL with the one Elastic Beanstalk gave you for your deployment
-YOUR_ELASTIC_BEANSTALK_URL = 'http://durranico.us-east-1.elasticbeanstalk.com/'
+# Force TLSv1.2
+create_urllib3_context(ssl_version=ssl.PROTOCOL_TLSv1_2)
 
-def test_record_and_process_endpoint():
-    # Using the first device index as an example. Modify this if needed.
-    payload = {
-        'device_index': 0
-    }
-    
-    response = requests.post(f"{YOUR_ELASTIC_BEANSTALK_URL}/record_and_process", json=payload)
-    
-    if response.status_code == 200:
-        print("Response received!")
-        data = response.json()
-        print("Text recognized:", data.get('text'))
-        print("GPT-4 response:", data.get('response'))
-    else:
-        print(f"Error {response.status_code}: {response.text}")
+# URL of your Heroku app. Replace this with your app's URL
+HEROKU_APP_URL = "https://therapai-4bfe081d185e.herokuapp.com/"
+
+def test_app():
+    # Assuming you'll have a 'test_audio.wav' file to test
+    with open('recording.wav', 'rb') as audio_file:
+        payload = {
+            'device_index': 1  # or whichever device index you want to test with
+        }
+        
+        files = {
+            'audio_file': audio_file
+        }
+        
+        response = requests.post(HEROKU_APP_URL, data=payload, files=files)
+        
+        if response.status_code == 200:
+            print("Response received:")
+            print(json.dumps(response.json(), indent=4))
+        else:
+            print(f"Failed with status code {response.status_code}:")
+            print(response.text)
 
 if __name__ == "__main__":
-    test_record_and_process_endpoint()
+    test_app()
