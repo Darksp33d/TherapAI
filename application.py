45df6116c400141e6722d0189ca4afa69154f4d4
Darksp33d <durrani.saeed1@gmail.com>
2023-10-09 16:32:59 -0400

    Moved db create into app context

--- application.py
+++ application.py
@@ -66,6 +66,7 @@ def get_gpt_response(user, input_text):
 
 
 if __name__ == "__main__":
-    db.create_all()  # This will ensure tables are created if they aren't there. Be cautious about using this in production.
+    with app.app_context():
+        db.create_all()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
