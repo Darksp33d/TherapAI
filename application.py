04188d7556035efa77d890bdcae8cd2a14de219e
Darksp33d <durrani.saeed1@gmail.com>
2023-10-09 20:14:58 -0400

    updated db inits

--- application.py
+++ application.py
@@ -72,10 +72,8 @@ def get_gpt_response(user, input_text):
 
     return response.choices[0].message['content'].strip()
 
-@app.before_first_request
-def initialize_database():
-    db.create_all()
-
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
