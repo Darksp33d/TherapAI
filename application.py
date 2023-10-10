2332b824d37491361b39dea1fcdde9d639256958
Darksp33d <durrani.saeed1@gmail.com>
2023-10-09 20:17:52 -0400

    added debugging

--- application.py
+++ application.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL.startswith("postgres://"):
